4d35e0b0b0e38a8a928ea42baeacac9b0138829b
Debmalya <debmalyapaine@gmail.com>
2020-01-25 18:18:02 +0530

    Updates

---
+++ precache-manifest.6d8f530b6e00324dbbbeb606ca075a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e51881762368360fd22abd048dbc9c6",
+    "url": "//debpaine.github.io/Face-Recognizer/./index.html"
+  },
+  {
+    "revision": "9a7ba4f43feaa45acc65",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/css/2.3497f09c.chunk.css"
+  },
+  {
+    "revision": "c52511a8c7961efb5eac",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/css/main.2def1995.chunk.css"
+  },
+  {
+    "revision": "9a7ba4f43feaa45acc65",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/js/2.7c3e70bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/js/2.7c3e70bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c52511a8c7961efb5eac",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/js/main.6dde518a.chunk.js"
+  },
+  {
+    "revision": "d045549cf8106885ebd0",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/js/runtime-main.7bd0f4b0.js"
+  },
+  {
+    "revision": "2e5504b004c6d213459414049486a9b7",
+    "url": "//debpaine.github.io/Face-Recognizer/./static/media/brain.2e5504b0.svg"
+  }
+]);
