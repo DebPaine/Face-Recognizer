5fb15ce2de6aef2fc5f361858820cba95ffc33e7
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-02-10 11:55:01 +0530

    changed image size

--- src/Components/FaceDetection/FaceDetection.js
+++ src/Components/FaceDetection/FaceDetection.js
@@ -6,7 +6,7 @@ const FaceDetection = ({ boxes, imageUrl }) => {
 	return (
 		<div className="mt2">
 			<div className="absolute imgcontainer">
-				<img id="inputImage" src={imageUrl} width="600px" height="auto" alt="" />
+				<img id="inputImage" src={imageUrl} width="500px" height="auto" alt="" />
 
 				{boxes.map((c, i) => <BoundingBox key={i} box={c} />)}
 			</div>
