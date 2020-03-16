b6873e8e0d6e22a2d922ce96cd7cd81a7a400088
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-16 12:48:56 +0530

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
-import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import 'bulma/css/bulma.css';
 import './fonts.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
