95732e89bec44ac0a4166460636100a6006036a7
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-16 17:48:01 +0530

    fixed bugs

--- Face-Recognizer-back-end/server.js
+++ Face-Recognizer-back-end/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('../build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve('../build', 'index.html'));
+		res.sendFile(path.join('../build', 'index.html'));
 	});
 }
 
