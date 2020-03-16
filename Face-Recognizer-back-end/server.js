175429c1b83c7f4c297c9ea404ddd7682afe3e24
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-16 17:14:53 +0530

    fixed bugs

--- Face-Recognizer-back-end/server.js
+++ Face-Recognizer-back-end/server.js
@@ -17,7 +17,7 @@ const knex = require('knex')({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
