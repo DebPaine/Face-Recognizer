f4d3d29f4feaf48866d8e9af33cc721defbb9605
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-20 10:22:42 +0530

    added responsive layout

--- frontend/src/Components/FaceDetection/FaceDetection.js
+++ frontend/src/Components/FaceDetection/FaceDetection.js
@@ -4,11 +4,9 @@ import BoundingBox from '../BoundingBox/BoundingBox';
 
 const FaceDetection = ({ boxes, imageUrl }) => {
 	return (
-		<div className='center width mt2'>
-			<div id='inputImage' className='absolute center width'>
-				<img src={imageUrl} alt='' />
-				{boxes.map((c, i) => <BoundingBox key={i} box={c} />)}
-			</div>
+		<div>
+			<img id='inputImage' className='center width mt2' src={imageUrl} alt='' />
+			{boxes.map((c, i) => <BoundingBox key={i} box={c} />)}
 		</div>
 	);
 };
